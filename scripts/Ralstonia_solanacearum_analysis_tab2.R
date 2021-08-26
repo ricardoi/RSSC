2520bf6f6ed7004e142a976cf2b629cf8fbda450
ricardoi <ralcala@ufl.edu>
2021-08-26 15:10:29 -0700

    data analysis

---
+++ scripts/Ralstonia_solanacearum_analysis_tab2.R
@@ -0,0 +1,15 @@
+#'@author Ricardo I Alcala for Kalpana Sharma
+#'@title RSSC - Summary statistics
+#'@description 
+#'
+
+#- setting working directory
+setwd("~/git_local/RSSC/")
+
+#----functions
+
+#------ loading libraries
+library(reshape2)
+library(ggplot2)
+library(igraph)
+library(bipartite)
