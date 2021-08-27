ed3d62c9edb8c0d2f8ca83690e1267c50469ab4f
ricardoi <ralcala@ufl.edu>
2021-08-27 11:53:07 -0700

    analysis results

--- scripts/Ralstonia_solanacearum_analysis_tab2.R
+++ scripts/Ralstonia_solanacearum_analysis_tab2.R
@@ -1,15 +1,28 @@
 #'@author Ricardo I Alcala for Kalpana Sharma
 #'@title RSSC - Summary statistics
-#'@description 
+#'@description
 #'
 
 #- setting working directory
 setwd("~/git_local/RSSC/")
 
-#----functions
+#----function
 
 #------ loading libraries
 library(reshape2)
 library(ggplot2)
 library(igraph)
 library(bipartite)
+
+#---- Loading data
+
+dat <- read.csv("data/Ralstonia_solanacearum_data_table2_2021fromR.csv")
+
+
+totals <- table(dat$Host, dat$phylotypes.Phylotype)
+total <- rbind(totals, colSums(totals))
+sum(totals)
+
+
+
+
